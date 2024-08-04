5e9bbb481924fd01bccfb65956674268ecac5101
Vankadari <AL71642@ad.wellpoint.com>
2024-08-04 16:10:39 +0530

    left navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import MyNavbar from './components/navbars/Navbar';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
   return (
--- src/components/navbars/Navbar.tsx
+++ src/components/navbars/Navbar.tsx
@@ -1,19 +1,60 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="dropdown show">
-       <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         Dropdown link
+    <div className='bg-primary d-flex flex-row justify-content-around' >
+      <div className="dropdown show">
+       <a className="btn dropdown-toggle text-light" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         For Employers
        </a>
 
-     <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+      <div className="dropdown-menu" data-toggle="dropup" aria-labelledby="dropdownMenuLink">
         <a className="dropdown-item" href="#">Action</a>
         <a className="dropdown-item" href="#">Another action</a>
         <a className="dropdown-item" href="#">Something else here</a>
+      </div>
      </div>
+     <div className="dropdown show">
+       <a className="btn dropdown-toggle text-light" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         For Producers
+       </a>
+
+      <div className="dropdown-menu" data-toggle="dropup" aria-labelledby="dropdownMenuLink">
+        <a className="dropdown-item" href="#">Action</a>
+        <a className="dropdown-item" href="#">Another action</a>
+        <a className="dropdown-item" href="#">Something else here</a>
+      </div>
+     </div>
+
+     <div className="dropdown show">
+       <a className="btn dropdown-toggle text-light" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         For Providers
+       </a>
+
+      <div className="dropdown-menu" data-toggle="dropup" aria-labelledby="dropdownMenuLink">
+        <a className="dropdown-item" href="#">Action</a>
+        <a className="dropdown-item" href="#">Another action</a>
+        <a className="dropdown-item" href="#">Something else here</a>
+      </div>
+     </div>
+
+     <div className='d-flex align-items-center'>
+       <a className='text-light' href='#'>
+         Medicaid Renewal
+       </a>
+     </div>
+
+
+     
+
     </div>
+
+    
+
+    
 
     
   )
